2b1dbaa71c30be326c786e5786af88b377d2f208
Katie <kab761@gmail.com>
2014-11-09 22:33:57 -0800

    added categories method to recipe model

--- katie/to_serve_man/app/models/recipe.rb
+++ katie/to_serve_man/app/models/recipe.rb
@@ -2,6 +2,19 @@ class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :ingredients
 
   def categories
-    ingredients.collect { |ingredient| ingredient.category }
+    hash = {}
+    ingredients.each do |ingredient|
+      if hash[ingredient.category]
+        hash[ingredient.category] << ingredient.name
+      else
+        hash[ingredient.category] = [ingredient.name]
+      end
+    end
+    hash
   end
+
+  def add(ingredient)
+    ingredients << ingredient
+  end
+
 end
--- katie/to_serve_man/spec/models/recipe_spec.rb
+++ katie/to_serve_man/spec/models/recipe_spec.rb
@@ -1,10 +1,22 @@
 require "rails_helper"
 
 describe "Recipe model" do
+  let(:gazpacho){Recipe.create(name:"Gazpacho", author:"Lisa", prep_time:60, cook_time:180, steps: "You don't win friends with salad", servings: 8)}
 
-  specify "I can run a test" do
-    expect(1).to eq 1
+  specify "recipe created with no ingredients" do
+    expect(gazpacho.ingredients.count).to eq 0
   end
 
-  
+  specify "can add ingredients to recipe" do
+    gazpacho.add(Ingredient.create(name:"Tomato", category:"Produce"))
+    expect(gazpacho.ingredients.count).to eq 1
+  end
+
+  specify "returns hash of ingredients keyed to categories" do
+    gazpacho.add(Ingredient.create(name:"Tomato", category:"Produce"))
+    gazpacho.add(Ingredient.create(name:"Vinegar", category:"Other"))
+    categories = {"Produce"=>["Tomato"], "Other"=>["Vinegar"]}
+    expect(gazpacho.categories).to eq categories
+  end
+
 end

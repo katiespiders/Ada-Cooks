37627c2902f49e76eabcd7ed7d120e5a2ca58eb1
Katie <kab761@gmail.com>
2014-11-10 01:57:48 -0800

    added ingredient model tests

--- katie/to_serve_man/spec/models/recipe_spec.rb
+++ katie/to_serve_man/spec/models/recipe_spec.rb
@@ -20,3 +20,19 @@ describe "Recipe model" do
   end
 
 end
+
+describe "Ingredient model" do
+  specify "returns all ingredients in a given category" do
+    t = Ingredient.create(name:"Tequila", category:"Other")
+    v = Ingredient.create(name:"Vodka", category:"Other")
+    n = Ingredient.create(name:"Non-alcoholic grapes", category:"Produce")
+    liquor = Ingredient.all_in("Other")
+
+    expect(liquor).to include v
+    expect(liquor).to include t
+    expect(liquor).not_to include n
+
+  end
+
+
+end

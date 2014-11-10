24eb08fd9a8acc542d4f31e727357052690a4f41
Katie <kab761@gmail.com>
2014-11-09 23:00:54 -0800

    started ingredients controller and views

--- katie/to_serve_man/app/controllers/ingredients_controller.rb
+++ katie/to_serve_man/app/controllers/ingredients_controller.rb
@@ -1,2 +1,25 @@
 class IngredientsController < ApplicationController
+  before_action :all_ingredients, only: [:index]
+  before_action :find_ingredient, only: [:show, :edit]
+
+  def create
+
+  end
+
+
+
+  private
+
+    def all_ingredients
+      @ingredients = Ingredient.all
+    end
+
+    def find_ingredient
+      @ingredient = Ingredient.find(params[:id])
+    end
+
+    def ingredient_params
+      params.require(:ingredients).permit(:name, :category)
+    end
+
 end
